1ad1a2743e83d4553f1f3a27c5c68db0de1af9a7
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2018-11-01 15:38:06 -0400

    Ex6: paddle.js: Added "//" before each comment

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -3,9 +3,10 @@
 // A class that defines how a paddle behaves, including the ability
 // to specify the input keys to move it up and down
 
-Paddle constructor
+/////////////FIXED! Added "//" before each comment
+//Paddle constructor
 
-Sets the properties with the provided arguments or defaults
+//Sets the properties with the provided arguments or defaults
 function Pladdle(x,y,w,h,speed,downKey,upKey) {
   this.x = x;
   this.y = y;
