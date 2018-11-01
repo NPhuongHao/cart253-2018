39b477ae83b024af24c9d666075e7dc0194de793
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2018-11-01 15:51:10 -0400

    Ex6: fixed paddle.y's constrain() syntax error

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -43,8 +43,8 @@ Paddle.prototype.handleInput = function() {
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
   this.y += this.vy;
-  ////////////FIXED! constraint() --> constrain()
-  this.y = constrain(this.y,0,hight-this.h);
+  ////////////FIXED! constraint() --> constrain(). hight --> height
+  this.y = constrain(this.y,0,height-this.h);
 }
 
 // display()
