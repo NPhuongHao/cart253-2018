3e5bb7573081c8c8b4dd5947d200140d09dd3b71
Phuong Hao Nguyen <36909625+NPhuongHao@users.noreply.github.com>
2018-11-01 15:39:11 -0400

    Ex6: Changed Pladdle() into Paddle()

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -7,7 +7,9 @@
 //Paddle constructor
 
 //Sets the properties with the provided arguments or defaults
-function Pladdle(x,y,w,h,speed,downKey,upKey) {
+
+////////////FIXED! Pladdle --> Paddle
+function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.x = x;
   this.y = y;
   this.xv = 0;
